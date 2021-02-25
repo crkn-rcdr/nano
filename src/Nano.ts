f6b63ea199d17a13b3eb70bccd09aaa29bee2223
Sascha Adler <sadler@crkn.ca>
2021-02-25 18:10:54 -0330

    docs: writes JSDoc definitions for Nano methods

--- src/Nano.ts
+++ src/Nano.ts
@@ -1,13 +1,22 @@
 import * as nano from "nano";
 
 interface AuthOptions {
-  user: string;
-  password: string;
+  readonly user: string;
+  readonly password: string;
 }
 
 type Scheme = "http" | "https";
 
+/**
+ * Class providing static methods for creating couchdb-nano instances.
+ */
 class Nano {
+  /**
+   * Create a couchdb-nano instance pointing to a CouchDB server. Basic authentication parameters can also be supplied.
+   * @param {string} url The CouchDB server to which nano requests will be sent.
+   * @param {AuthOptions} [auth] Basic authentication parameters.
+   * @returns {nano.ServerScope} The nano instance.
+   */
   static get(url: string, auth?: AuthOptions): nano.ServerScope {
     try {
       new URL(url);
@@ -32,7 +41,18 @@ class Nano {
     });
   }
 
-  static localhost(port: number, auth?: AuthOptions, scheme: Scheme = "http") {
+  /**
+   * Create a couchdb-nano instance pointing to a CouchDB server running locally, at a given port. Basic authentication parameters can also be supplied.
+   * @param {number} port Port on the local machine CouchDB is being served at.
+   * @param {AuthOptions} [auth] Basic authentication parameters.
+   * @param {Scheme} [scheme="http"] URL scheme to use. "http" and "https" are supported.
+   * @returns {nano.ServerScope} The nano instance.
+   */
+  static localhost(
+    port: number,
+    auth?: AuthOptions,
+    scheme: Scheme = "http"
+  ): nano.ServerScope {
     return Nano.get(`${scheme}://localhost:${port}/`, auth);
   }
 }
